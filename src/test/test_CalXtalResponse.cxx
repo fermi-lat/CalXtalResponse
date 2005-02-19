97c4ebd86e809c10ceab8f70cd6844695615bfba
fewtrell <>
2005-02-19 02:17:45 +0000

    fixed test program to work w/ last update (forgot to test last time  - whoops!)

--- src/test/test_CalXtalResponse.cxx
+++ src/test/test_CalXtalResponse.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/CalXtalResponse/src/test/test_CalXtalResponse.cxx,v 1.6 2004/08/27 22:04:22 fewtrell Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalXtalResponse/src/test/test_CalXtalResponse.cxx,v 1.1.1.1 2005/02/05 09:56:10 fewtrell Exp $
 
 // Include files
 // Gaudi system includes
@@ -212,19 +212,23 @@ StatusCode test_CalXtalResponse::test_xtalEneTool() {
   CalXtalId xtalId(0,2,3,face,rng);
   float energy;
 
-  bool below_thresh;
-  if ((sc = m_xtalEneTool->calculate(xtalId, rng, rng, 1000, 1000, energy, below_thresh))
+  bool xtalBelowThresh=false, rngBelowThresh=false;
+  if ((sc = m_xtalEneTool->calculate(xtalId, rng, rng, 
+                                     1000, 1000, energy, 
+                                     rngBelowThresh,
+                                     xtalBelowThresh))
       != sc) {
     msglog << MSG::ERROR << "Error calling XtalEneTool::calculate()" << endreq;
     return sc;
   }
   msglog << MSG::INFO << "2 face energy calculated:" << energy << 
-    " below_thresh=" << below_thresh<< endreq;
+    " below_thresh="  << xtalBelowThresh << endreq;
 
-  if ((sc = m_xtalEneTool->calculate(xtalId, 1000, 0, energy, below_thresh))
+  if ((sc = m_xtalEneTool->calculate(xtalId, 1000, 0, energy, 
+                                     rngBelowThresh, xtalBelowThresh))
       != sc) {
     msglog << MSG::ERROR << "Error calling XtalEneTool::calculate()" 
-      " below_thresh=" << below_thresh<< endreq;
+      " below_thresh="   << xtalBelowThresh << endreq;
     return sc;
   }
   msglog << MSG::INFO << "1 face energy calculated:" << energy << endreq;
