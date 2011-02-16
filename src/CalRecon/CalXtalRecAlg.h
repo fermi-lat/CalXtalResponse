6eb3f25f0c76b313ee4c57854ad3f91a1f12f50b
usher <>
2011-02-16 16:55:54 +0000

    Updating before tagging

--- src/CalRecon/CalXtalRecAlg.h
+++ src/CalRecon/CalXtalRecAlg.h
@@ -1,6 +1,6 @@
 #ifndef CalXtalRecAlg_h
 #define CalXtalRecAlg_h 
-//    $Header: /nfs/slac/g/glast/ground/cvs/CalXtalResponse/src/CalRecon/CalXtalRecAlg.h,v 1.2 2007/11/06 20:54:00 fewtrell Exp $
+//    $Header: /nfs/slac/g/glast/ground/cvs/CalXtalResponse/src/CalRecon/CalXtalRecAlg.h,v 1.3 2008/01/22 20:14:48 fewtrell Exp $
 /** @file
     @author Z.Fewtrell
 */
